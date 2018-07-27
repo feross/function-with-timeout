78209cef650a4376e058af39bfa30828efeb2149
Feross Aboukhadijeh <feross@feross.org>
2018-07-27 00:39:28 -0700

    test: more verbosity

--- test/basic.js
+++ test/basic.js
@@ -12,44 +12,48 @@ test('function never gets called manually', function (t) {
 })
 
 test('function gets called before timeout', function (t) {
-  t.plan(1)
+  t.plan(2)
   var d = Date.now()
   function myFn () {
     var elapsed = Date.now() - d
-    t.ok(elapsed >= 100 && elapsed < 1000)
+    t.ok(elapsed >= 100)
+    t.ok(elapsed < 1000)
   }
   var fn = functionWithTimeout(myFn)
   setTimeout(fn, 100)
 })
 
 test('function gets called after timeout', function (t) {
-  t.plan(1)
+  t.plan(2)
   var d = Date.now()
   function myFn () {
     var elapsed = Date.now() - d
-    t.ok(elapsed >= 1000 && elapsed < 1500)
+    t.ok(elapsed >= 1000)
+    t.ok(elapsed < 1500)
   }
   var fn = functionWithTimeout(myFn)
   setTimeout(fn, 1500)
 })
 
 test('function gets called before timeout (custom timeout)', function (t) {
-  t.plan(1)
+  t.plan(2)
   var d = Date.now()
   function myFn () {
     var elapsed = Date.now() - d
-    t.ok(elapsed >= 100 && elapsed < 200)
+    t.ok(elapsed >= 100)
+    t.ok(elapsed < 200)
   }
   var fn = functionWithTimeout(myFn, 200)
   setTimeout(fn, 100)
 })
 
 test('function gets called after timeout (custom timeout)', function (t) {
-  t.plan(1)
+  t.plan(2)
   var d = Date.now()
   function myFn () {
     var elapsed = Date.now() - d
-    t.ok(elapsed >= 500 && elapsed < 20 * 1000)
+    t.ok(elapsed >= 500)
+    t.ok(elapsed < 20 * 1000)
   }
   var fn = functionWithTimeout(myFn, 500)
   setTimeout(fn, 1000)
